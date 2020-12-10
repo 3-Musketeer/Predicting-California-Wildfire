ccfae336b0037038f102f15edb94131ec3617994
Shiv Kumar Ganesh <gshiv.sk@gmail.com>
2020-12-09 16:18:06 -0800

    Updated the ML Dashboard

--- ml_dashboard.py
+++ ml_dashboard.py
@@ -3,7 +3,6 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from joblib import load
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -11,7 +10,6 @@ import sklearn
 import pickle
 from sklearn.preprocessing import StandardScaler
 
-sns.set_style('darkgrid')
 
 # DATA SOURCES
 FILTERED_CALI_DATA_WITH_COUNTY = './Data/USW00003171.csv'
@@ -35,120 +33,6 @@ def load_data(nrows, source):
 #     ])
 
 # 'You selected:', option
-
-
-def introduction_func():
-    st.title('Wildfire Prediction')
-    """
-    # Introduction to Data Sources
-    We will be importing two datasets here. The first one represents the Dataset that is collected from various resources and the weather data set is downloaded from the **RIVERSIDE MUNICIPAL AIRPORT, CA US** Weather Center(This station can be found on [NOAA Website](https://www.ncdc.noaa.gov/cdo-web/datasets/GHCND/stations/GHCND:USW00003171/detail) ). The data available in these weather station range from April,1998 to current date. 
-
-    Considering **Riverside County** for our analysis is based on the data and visualizations that we see below. Lets go through the various data sources.
-    """
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-
-    # Load 100 rows of Wildfire data into the dataframe.
-    data = load_data(100, WILDFIRE_DATA_URL)
-    st.subheader('Raw Wildfire Data')
-    st.write(data)
-
-    # Load 100 rows of Wildfire data into the dataframe.
-    data = load_data(100, FILTERED_CALI_DATA_WITH_COUNTY)
-    st.subheader('Raw Weather Data')
-    st.write(data)
-
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text('Loading data...done!')
-
-    """
-    Data was extracted from the above two data sets from the year 1998 to year 2015 and Joined to form
-    a consolidated dataset. This is available in the Exploratory Data Analysis Section.
-    """
-
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-
-    # Load 100 rows of Weather data into the dataframe.
-    data = load_data(100, WEATHER_DATA_URL)
-    st.subheader('Raw Consolidated Data')
-    st.write(data)
-
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text('Loading data...done!')
-
-
-def exploratory_analysis():
-    # Data import for California Wildfire Data
-    dfCaliData = pd.read_csv(WILDFIRE_DATA_URL)
-
-    st.title('Exploratory Data Analysis for Wildfire')
-
-    # Select the columns required for our analysis leaving others
-    dfCaliData = dfCaliData[['DISCOVERY_DATE', 'DISCOVERY_TIME',
-                             'STAT_CAUSE_DESCR', 'FIRE_SIZE', 'FIRE_SIZE_CLASS', 'NewCountyValue']]
-
-    # Lets convert the Discovery Date to Proper Date
-    dfCaliData['DATE'] = pd.to_datetime(
-        dfCaliData['DISCOVERY_DATE'] - pd.Timestamp(0).to_julian_date(), unit='D')
-
-    # Adding Month Column and Day of the week in order to Aid visualization
-    dfCaliData['MONTH'] = pd.DatetimeIndex(dfCaliData['DATE']).month
-    dfCaliData['DAY_OF_WEEK'] = dfCaliData['DATE'].dt.strftime("%A")
-
-    # Drop the DISCOVERY_DATE column as the DATE column already holds the required data
-    dfCaliData = dfCaliData.drop(['DISCOVERY_DATE'], axis=1)
-
-    # Graphical representation of the above data
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        st.markdown(
-            f"""
-        <style>
-            .reportview-container .main .block-container{{
-                max-width: {100}%;
-                padding-top: {1}rem;
-                padding-right: {1}rem;
-                padding-left: {1}rem;
-                padding-bottom: {1}rem;
-            }}
-        </style>
-        """,
-            unsafe_allow_html=True,
-        )
-        """
-        ### Cause of Wildfire in California
-        We will check the frequency of the wildfire with the causes that caused it.
-        Lets get the frequency count for the Cause of the wild fire
-        """
-        fig = sns.displot(data=dfCaliData, y="STAT_CAUSE_DESCR", hue="STAT_CAUSE_DESCR",
-                          multiple="stack").set(title='Causes of Wildfire across California')
-        plt.xlabel('Number of incidents')
-        plt.ylabel('Causes of Wildfire Incidents')
-        st.pyplot(fig)
-
-        """
-        From the above data as well as the Graph it is evident that the major cause of wildfire is **Miscellaneous(unknown)** whereas the second largest cause is **Equipement usage(Caused by human intervention)**. As the list suggests **Lightening**, **Arson** and **Debris Burning** also contribute to the cause.
-        """
-
-    with col2:
-        """
-        ### Most active Months
-
-        The monthly analysis will show us which months are prone to wildfire incidents. These wildfire incidents increase from a period of May to Septemper, which mostly is a dry spell in California. We should also note that these months are the dry months in california.
-        """
-        # Graphical representation of the above data
-        dfMonth = dfCaliData.groupby(['MONTH']).size()
-        dfMonth = dfMonth.reset_index(name='count').sort_values('count')
-
-        # Creating barplot
-        fig, ax = plt.subplots()
-        ax = sns.barplot(data=dfMonth, y='count', x='MONTH').set(
-            title='Wildfire per Month')
-        plt.xlabel('Month of the year')
-        plt.ylabel('Number of Wildfire Incidents')
-        st.pyplot(ax)
-
 
 def predict_wildfire():
 
